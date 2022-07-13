c43f3348e637e6e2714eed7d8f0a05bd49f4e2ff
Iris Stream <ngodaisonn@gmail.com>
2022-07-13 10:51:49 +0700

    Create Gitlab class

---
+++ src/s68/devops/petclinic/Gitlab.groovy
@@ -0,0 +1,58 @@
+class Gitlab{
+	def UpdateRepo(){
+		if(isMergeRequest()){
+			checkout changelog: true, poll: true, scm: [
+				$class: 'GitSCM',
+				branches: [[name: "origin/${env.gitlabSourceBranch}"]],
+				extensions: [
+					[
+						$class: 'PreBuildMerge', 
+						options: [
+							fastForwardMode: 'FF', 
+							mergeRemote: 'origin', 
+							mergeStrategy: 'DEFAULT', 
+							mergeTarget: "${env.gitlabTargetBranch}"
+						]
+					]
+				],
+				userRemoteConfigs: [[name: 'origin', url: "${env.gitlabSourceRepoSshUrl}"]]
+			]
+		}
+		else{
+			checkout changelog: true,poll: true, scm: [
+				$class: 'GitSCM',
+				branches: [[ name: "${env.gitlabAfter}" ]],
+				userRemoteConfigs: [[name: 'origin', url: "${env.gitlabSourceRepoSshUrl}"]],
+			]
+		}
+	}
+
+	def isMergeRequest(){
+		try {
+			gitlabMergeRequestIId
+			true
+		} catch (e) {
+			return false
+		}
+	}
+
+	def getMergeRequestInfo(){
+		"PR-${getMergeRequestId()}, branch: ${(getSourceBranch())} by ${getMergeRequestByFullName()}\n${getMergeRequestUrl()}"
+	}
+
+	def getMergeRequestUrl(){
+		"${gitlabTargetRepoHttpUrl.minus('.git')}/merge_requests/$gitlabMergeRequestIId"
+	}
+
+	def getMergeRequestId(){
+		gitlabMergeRequestIId
+	}
+
+	def getSourceBranch(){
+		gitlabSourceBranch
+	}
+
+	def getMergeRequestByFullName(){
+		gitlabUserName
+	}
+}
