dc501df34e612c6a344186bfb962686209dd5099
Iris Stream <ngodaisonn@gmail.com>
2022-07-13 14:11:09 +0700

    add gitlab vars

---
+++ vars/gitlab.groovy
@@ -0,0 +1,68 @@
+def updateRepo(){
+	if(isMergeRequest()){
+		checkout changelog: true, poll: true, scm: [
+			$class: 'GitSCM',
+			branches: [[name: "origin/${getSourceBranch()}"]],
+			extensions: [
+				[
+					$class: 'PreBuildMerge', 
+					options: [
+						fastForwardMode: 'FF', 
+						mergeRemote: 'origin', 
+						mergeStrategy: 'DEFAULT', 
+						mergeTarget: "${getTargetBranch()}"
+					]
+				]
+			],
+			userRemoteConfigs: [[name: 'origin', url: "${getSourceUrl()}"]]
+		]
+	}
+	else{
+		checkout changelog: true,poll: true, scm: [
+			$class: 'GitSCM',
+			branches: [[ name: "${getCommitId()}" ]],
+			userRemoteConfigs: [[name: 'origin', url: "${getSourceRepoUrl()}"]],
+		]
+	}
+}
+
+def isMergeRequest(){
+	try {
+		gitlabMergeRequestIId
+		true
+	} catch (e) {
+		return false
+	}
+}
+
+def getMergeRequestInfo(){
+	"PR-${getMergeRequestId()}, branch: ${(getSourceBranch())} by ${getMergeRequestByFullName()}\n${getMergeRequestUrl()}"
+}
+
+def getMergeRequestUrl(){
+	"${gitlabTargetRepoHttpUrl.minus('.git')}/merge_requests/$gitlabMergeRequestIId"
+}
+
+def getMergeRequestId(){
+	gitlabMergeRequestIId
+}
+
+def getSourceBranch(){
+	gitlabSourceBranch
+}
+
+def getTargetBranch(){
+	gitlabTargetBranch
+}
+
+def getMergeRequestByFullName(){
+	gitlabUserName
+}
+
+def getRepoUrl(){
+	gitlabSourceRepoHttpUrl
+}
+
+def getCommitId(){
+	gitlabAfter
+}
