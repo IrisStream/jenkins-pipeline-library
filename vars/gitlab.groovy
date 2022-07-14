e4ff1e9bc10790ba616870e0c4dcb1d420aee80f
Iris Stream <ngodaisonn@gmail.com>
2022-07-14 10:11:38 +0700

    Remove unuse code

--- vars/gitlab.groovy
+++ vars/gitlab.groovy
@@ -1,38 +1,36 @@
 def updateRepo(){
-	withCredentials([string(credentialsId: 'git-credential', variable: 'GIT_CREDENTIAL']){
-		if(isMergeRequest()){
-			checkout changelog: true, poll: true, scm: [
-				$class: 'GitSCM',
-				branches: [[name: "origin/${env.gitlabSourceBranch}"]],
-				extensions: [
-					[
-						$class: 'PreBuildMerge', 
-						options: [
-							fastForwardMode: 'FF', 
-							mergeRemote: 'origin', 
-							mergeStrategy: 'DEFAULT', 
-							mergeTarget: "${env.gitlabTargetBranch}"
-						]
+	if(isMergeRequest()){
+		checkout changelog: true, poll: true, scm: [
+			$class: 'GitSCM',
+			branches: [[name: "origin/${env.gitlabSourceBranch}"]],
+			extensions: [
+				[
+					$class: 'PreBuildMerge', 
+					options: [
+						fastForwardMode: 'FF', 
+						mergeRemote: 'origin', 
+						mergeStrategy: 'DEFAULT', 
+						mergeTarget: "${env.gitlabTargetBranch}"
 					]
-				],
-				userRemoteConfigs: [[
-					name: 'origin', 
-					url: "${env.gitlabSourceRepoSshUrl}",
-					credentialsId: 'git-credential'
-				]]
-			]
-		}
-		else{
-			checkout changelog: true,poll: true, scm: [
-				$class: 'GitSCM',
-				branches: [[ name: "${env.gitlabAfter}" ]],
-				userRemoteConfigs: [[
-					name: 'origin', 
-					url: "${env.gitlabSourceRepoSshUrl}",
-					credentialsId: 'git-credential'
-				]]
-			]
-		}
+				]
+			],
+			userRemoteConfigs: [[
+				name: 'origin', 
+				url: "${env.gitlabSourceRepoSshUrl}",
+				credentialsId: 'git-credential'
+			]]
+		]
+	}
+	else{
+		checkout changelog: true,poll: true, scm: [
+			$class: 'GitSCM',
+			branches: [[ name: "${env.gitlabAfter}" ]],
+			userRemoteConfigs: [[
+				name: 'origin', 
+				url: "${env.gitlabSourceRepoSshUrl}",
+				credentialsId: 'git-credential'
+			]]
+		]
 	}
 }
 
