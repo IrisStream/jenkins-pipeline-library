e9c0497e9d5eb5470cccda0e7157e0cf949aa91c
Iris Stream <ngodaisonn@gmail.com>
2022-07-14 12:09:03 +0700

    add tester module

---
+++ vars/tester.groovy
@@ -0,0 +1,21 @@
+def testSource(){
+	stage("Code Quality Check"){
+		echo "Code Quality Check"
+		dir(SOURCE_DIR){
+			withSonarQubeEnv(installationName: 'spring-petclinic'){
+				echo "Quality Check"
+				sh "./jenkins_scripts/quality_check.sh"
+			}
+		}
+	}
+	stage("Quality Gate"){
+		echo "Quality Gate"
+		timeout(time: 20, unit: "MINUTES"){ 
+			waitForQualityGate abortPipeline: true
+		}
+	}
+	stage("Unit Test"){
+		echo "Unit Test"
+		sh "./jenkins_scripts/unit_test.sh"
+	}
+}
