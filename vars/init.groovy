7d7af89b1dd74ff3e6a6a961bab8c209dd14a40b
Iris Stream <ngodaisonn@gmail.com>
2022-07-15 10:05:50 +0700

    Fix loading resources

--- vars/init.groovy
+++ vars/init.groovy
@@ -1,5 +1,6 @@
-def vars
+import groovy.json.*
 def call(){
     def settings = libraryResource 's68/devops/petclinic/jenkins-settings.json'
-    var = readJSON text: settings
+    def var = readJSON text: settings
+    return var
 }
